221b346823791a9ed7af017e7826bd414f3c8728
Anton <antonkekdurachek@gmail.com>
2022-08-20 13:11:14 +0300

    add modal form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 "use strict"
 
 
-$(function(){
+$(function () {
 
   //SKIDER
   $('.body-slider').slick({
@@ -13,203 +13,64 @@ $(function(){
 
   })
 
-  $('.left__arrow').click(function(){
+  $('.left__arrow').click(function () {
     $('.body-slider').slick('slickPrev');
   })
-  
-  $('.right__arrow').click(function(){
+
+  $('.right__arrow').click(function () {
     $('.body-slider').slick('slickNext');
   })
 })
+
+
+
+
+
+/////footer
+
+const btnForm = $('#btnForm')
+const modalForm = $('.form')
+const closeBtn = $('.btn-primary')
+
+modalForm.slideUp()
+
+$(btnForm).on('click', function () {
+  console.log("click")
+  modalForm.slideToggle()
+  btnForm.slideToggle()
+});
+
+$(closeBtn).on('click', function () {
+  console.log("click")
+  btnForm.slideToggle()
+  modalForm.slideToggle()
+
+});
+
+
+
+
+///validation 
+$("#myForm").validate({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    },
+    name: {
+    required: true,
+      minlength: 5  
+    }
     
-//GOOGLE MAPS
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-          lat: 23.331400884730492,
-          lng: 114.1726722866911,
-      },
-      zoom: 2,
-      styles: [
-        {
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.land_parcel",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#bdbdbd"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#ffffff"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#dadada"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#c9c9c9"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        }
-      ],
-  });
-  const menu = $("#menu")
-
-  let HongKongPsn = {lat: 22.331400884730492, lng:114.1726722866911};
-
-  let showPnt = {
-    zoom: 20,
-    center: HongKongPsn
-  };
-
-  let HongKongMarker = new google.maps.Marker({
-      position: HongKongPsn,
-      map,
-      title: "Hong Kong",
-    });
-    HongKongMarker.setMap(map)
-
-   HongKongMarker.addListener('onclick', showPnt) 
-}
-
-
-    initMap()
+    },
+  messages: {
+    email: {
+      required: "Поле email обязательно для заполнения",
+    },
+  name: {
+    required: "Поле имя обязательно для заполнения",
+    minlength: jQuery.validator.format("Длина имени должна быть больше 5-ти символов")
+  }
+  },
+   
+});
